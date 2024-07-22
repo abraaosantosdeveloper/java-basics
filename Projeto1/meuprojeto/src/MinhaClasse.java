1dece9550b2f4c70d6a4cd22569a9f61f742232f
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-22 19:10:25 -0300

    Exibição de mensagem

--- Projeto1/meuprojeto/src/MinhaClasse.java
+++ Projeto1/meuprojeto/src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+
+        int idade = 20;
+        double salario = 1234.56;
+        char letra = 'A';
+        boolean verdadeiro = true;
+        String frase = "Olá, mundo!";
+        final String NOME = "Abraão Santos";
+        String profissao = "Desenvolvedor Java";
+        
+        System.out.print(frase + " Eu sou " + NOME + ", e tenho " + idade + " anos. \nTrabalho como " + profissao);
+
     }
 }
