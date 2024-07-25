01fdbe4c56c6e2fd6e775467984742ef10ae76c7
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-24 21:11:19 -0300

    New method testing for print username with concat() and operator +

--- Projetos/meuprojeto/src/dio/cursodejava/segundasemana/ClasseNomeCompleto.java
+++ Projetos/meuprojeto/src/dio/cursodejava/segundasemana/ClasseNomeCompleto.java
@@ -2,5 +2,20 @@ package dio.cursodejava.segundasemana;
 
 
 public class ClasseNomeCompleto {
-    
+
+    public static void main(String[] args) {
+        String nome = "Abraao";
+        String sobrenome = "Santos";
+        String nomeCompleto = concatenarNomes(nome, sobrenome);
+        String nomeCompleto2 = concatenarNomes2(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+        System.out.println(nomeCompleto2);
+    }
+    public static String concatenarNomes(String nome, String sobrenome){
+        return "Resultado do método: " + nome + " " + sobrenome;
+    }
+    public static String concatenarNomes2(String nome, String sobrenome){
+        return "Resultado do método utilizando Concat(): " + nome.concat(" ").concat(sobrenome);
+    }
 }
