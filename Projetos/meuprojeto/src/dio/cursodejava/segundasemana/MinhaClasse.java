ca77671f8cf410e11c4c396a4140474733d0fdd5
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-24 21:00:43 -0300

    Package moved

--- Projetos/meuprojeto/src/dio/cursodejava/segundasemana/MinhaClasse.java
+++ Projetos/meuprojeto/src/dio/cursodejava/segundasemana/MinhaClasse.java
@@ -6,12 +6,26 @@ public class MinhaClasse {
         // Variáveis não podem começar com caracteres especiais além de _ ou $, tampouco
         // números.
         // Não pode começar ou conter caracteres epeciais.
+
+        // Inteiro
         int idade = 20;
+
+        // Double (Decimais)
         double salario = 1234.56;
+
+        // Caractere
         char letra = 'A';
+
+        // Booleano(True/False)
         boolean verdadeiro = true;
+
+        // String (Texto)
         String frase = "Olá, mundo!";
+
+        // Constante (Valor fixo)
         final String NOME = "Abraão Santos";
+
+        // Variável com nome composto
         String profissao = "Desenvolvedor Java";
 
         System.out.print(frase + " Eu sou " + NOME + ", e tenho "
