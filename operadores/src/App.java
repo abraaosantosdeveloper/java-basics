72cff9606f443a45fbdd6aafaa584df414e68fde
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-28 16:08:20 -0300

    Testings made

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -1,8 +1,13 @@
 public class App {
     public static void main(String[] args){
-        String nome = "Abraão";
-        String sobrenome = "Santos";
-        String nomeCompleto = nome + " " + sobrenome;
-        System.out.println(nomeCompleto);
+        // String nome = "Abraão";
+        // String sobrenome = "Santos";
+        // String nomeCompleto = nome + " " + sobrenome;
+        // System.out.println(nomeCompleto);
+
+
+        int numero = 5;
+
+        System.out.println(++numero); // pré incremento de numero
     }
 }
