78fcc5515317406ab719a818dfbfa98d70998618
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-28 16:04:07 -0300

    Modified App.java

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World...");
+    public static void main(String[] args){
+        String nome = "Abraão";
+        String sobrenome = "Santos";
+        String nomeCompleto = nome + " " + sobrenome;
+        System.out.println(nomeCompleto);
     }
 }
