4da025309543e86c0659b82f58aacf48676e4120
Abraao Santos <abraaofilipi12@gmail.com>
2024-07-28 16:25:18 -0300

    Added: operador ternário

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -6,8 +6,20 @@ public class App {
         // System.out.println(nomeCompleto);
 
 
-        int numero = 5;
+        // int numero = 5;
 
-        System.out.println(++numero); // pré incremento de numero
+        // System.out.println(++numero); // pré incremento de numero
+
+        // Operador ternário: <Expressão condicional> ? <Condição Verdadeira> : <Condição falsa>
+
+        int numero = 2;
+        int numero2 = 5;
+
+        int soma = numero + numero2;
+
+        String resultado = (soma > 10) ? "soma é maior que 10" : "Soma é menor que 10";
+
+        System.out.println(resultado);
+
     }
 }
